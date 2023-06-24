24826d17690080c18170c29771f64f8f065a6c7e
dallen1 <dallen1@epscor.unm.edu>
2023-06-24 08:12:37 -0600

    rest service class

---
+++ index.js
@@ -0,0 +1,64 @@
+class RESTService {
+    static url = 'http://localhost:3000/bands'
+
+    static async getAllBands() {      
+        const response = await fetch(RESTService.url);
+        const jsonData = await response.json();
+        return jsonData;
+    
+    }
+
+    static async getBand(id) {
+        const response = await fetch(RESTService.url + `/${id}`);
+        const jsonData = await response.json();
+        return jsonData;
+    }
+
+    static async createBand(band) {
+        try {
+            const response = await fetch(RESTService.url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(band),
+            });
+        
+            const result = await response.json();
+            console.log("Success:", result);
+        } catch (error) {
+            console.error("Error:", error);
+        }
+        
+        
+        const data = { name: band, members: [] };
+        return data;
+    }
+
+    static async updateBand(band) {
+    try {
+        const response = await fetch(RESTService.url+ `/${band.id}`, {
+        method: "PUT", 
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(band),
+        });
+
+        const result = await response.json();
+        console.log("Success:", result);
+        } catch (error) {
+            console.error("Error:", error);
+        }
+
+
+        const data = { name: band, members: [] };
+        return data;
+    }
+
+    static async deleteBand(id) {
+        const response = await fetch(RESTService.url + `/${id}`, {method: "DELETE"});
+        const jsonData = await response.json();
+        return jsonData;
+    }
+}
